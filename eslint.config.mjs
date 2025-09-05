0833e927bd98aaa5ae45148998ba1586d7a16ead
abdout <osmanabdout@hotmail.com>
2025-09-05 16:45:20 +0300

    x

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,6 +21,12 @@ const eslintConfig = [
       "build/**/*",
     ],
   },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
